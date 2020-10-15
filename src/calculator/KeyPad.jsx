a81efa6f3c0ba7149bc90e79e8d0c8f80da53fc8
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-15 18:31:29 +0800

    Removes unnecessary codes

--- src/calculator/KeyPad.jsx
+++ src/calculator/KeyPad.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Calculator from './Calculator'
 const KeyPad = ({keyButton: {label, id, clasname, action}, renderNumber}) => {
-  console.log(id)
   return (
       <button className={clasname} onClick={(e)=>renderNumber(id, label, action)}>{label}</button>  
   )
