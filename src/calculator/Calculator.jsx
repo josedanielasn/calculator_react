ae6ae2d0550dada2f8ef1819a7d1c0edf67d9a59
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-10-16 09:16:33 +0800

    Removes unnecessary comments and deletes transferred codes

--- src/calculator/Calculator.jsx
+++ src/calculator/Calculator.jsx
@@ -2,30 +2,7 @@ import React, { useState } from 'react'
 import KeyPad from './KeyPad'
 import Display from './Display'
 import UpperDisplay from './UpperDisplay'
-import userEvent from '@testing-library/user-event'
 const Calculator = () => {
-  const keyButtons = [
-    {clasname:'keys' ,id: 'AC', label: 'AC', action:'clear'},
-    {clasname:'keys' ,id: 'double', label: '+/-',action:'none'},
-    {clasname:'keys' ,id: 'modulo', label: '%',action:'module'},
-    {clasname:'keys -orange' ,id: 'divide', label: '÷',action:'divide'},
-    {clasname:'keys' ,id: 7, label: '7',action:'number'},
-    {clasname:'keys' ,id: 8, label: '8',action:'number'},
-    {clasname:'keys' ,id: 9, label: '9',action:'number'},
-    {clasname:'keys -orange' ,id: 'multiply', label: '×',action:'multiply'},
-    {clasname:'keys' ,id: 4, label: '4',action:'number'},
-    {clasname:'keys' ,id: 5, label: '5',action:'number'}, 
-    {clasname:'keys' ,id: 6, label: '6',action:'number'},
-    {clasname:'keys -orange' ,id: 'subtract', label: '-',action:'subtract'},
-    {clasname:'keys' ,id: 1, label: '1',action:'number'},
-    {clasname:'keys' ,id: 2, label: '2',action:'number'},
-    {clasname:'keys' ,id: 3, label: '3', action:'number'},
-    {clasname:'keys -orange' ,id: 'add', label: '+',action:'add'},
-    {clasname:'keys -zero' ,id: 0, label: '0',action:'number'},
-    {clasname:'keys' ,id: 'decimal', label: '.',action:'decimal'},
-    {clasname:'keys -orange -equal' ,id: 'equal', label: '=', action:'calculate'},
-
-  ]
 
   const[display, setDisplay] = useState('0')
   const[lastButton, setLastButton] = useState('number')
@@ -77,21 +54,19 @@ const Calculator = () => {
   }
 
   const calculate = (n1,n2 = 0) =>{
-    let result = '';
+    let result
     if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2); 
+      result = parseFloat(n1) + parseFloat(n2) 
     } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2);
+      result = parseFloat(n1) - parseFloat(n2)
     } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2);
+      result = parseFloat(n1) * parseFloat(n2)
     } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2);
+      result = parseFloat(n1) / parseFloat(n2)
     } else{
       result = 0
     }
-
-    result = parseFloat(result.toFixed(6))
-    return (result.toString().length > 14 ? 'Number is too big': result);
+    return result
   }
 
 
@@ -100,10 +75,7 @@ const Calculator = () => {
       <UpperDisplay/>
       <Display displayNum={display}/>
      <div className ='calculator -keys'>
-      {keyButtons.map((keyButton) => {
-        // console.log(keyButton)
-        return <KeyPad renderNumber={actionForButtons} key={keyButton.id} keyButton={keyButton}/>
-      })}
+      <KeyPad renderNumber={actionForButtons}/>
     </div> 
     </div>
 
